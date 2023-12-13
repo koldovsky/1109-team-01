ac09bd29562209712e76b975f95ef1b91e564f9a
Aleksandr <some_mail@mail.com>
2023-12-13 16:32:46 +0200

    ...

---
+++ js/equipment_carousel.js
@@ -0,0 +1,8 @@
+console.log(1)
+// const arrowsBtns = document.querySelectorAll(".equipment__carousel i");
+
+// arrowsBtns.forEach(btn => {
+//     btn.addEventListener("click", () => {
+//       console.log(btn.id);
+//     })
+// });
