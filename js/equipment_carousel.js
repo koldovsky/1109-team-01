6feea418d4404ea6e9cec498c2facb3351d19a2b
Aleksandr <some_mail@mail.com>
2023-12-13 16:59:25 +0200

    change properties for buttons in slider

--- js/equipment_carousel.js
+++
@@ -1,8 +0,0 @@
-console.log(1)
-// const arrowsBtns = document.querySelectorAll(".equipment__carousel i");
-
-// arrowsBtns.forEach(btn => {
-//     btn.addEventListener("click", () => {
-//       console.log(btn.id);
-//     })
-// });
