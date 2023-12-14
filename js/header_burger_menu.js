85ba10067ad103a1d35d8c06c7b8a9e4fbd69472
Kyrylo Suprun <Creatorsk@gmail.com>
2023-12-14 20:53:08 +0000

    Upgrade header-navbar & delivery status: in progress...

---
+++ js/header_burger_menu.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    var menuContainer = document.querySelector('.header__menu-container');
+    menuContainer.style.display = (menuContainer.style.display === 'block') ? 'none' : 'block';
+}
