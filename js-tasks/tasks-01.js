b72f80dbdfce2e522774c43bab6d31fcb1f0fe11
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2023-12-15 20:02:44 +0200

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89/train/javascript
+
+function mouthSize(animal) {
+  if (animal.toLowerCase() === "alligator") {
+    return "small";
+  } else {
+    return "wide";
+  }
+}
+
+
