085aeb84f32f35085dc2136bb70a268c25d99589
SawaAser <144454570+SawaAser@users.noreply.github.com>
2023-12-16 00:40:46 +0200

    comment

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -10,32 +10,32 @@ function mouthSize(animal) {
 
 
 
-1.Grasshopper - Messi goals function - https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+// 1.Grasshopper - Messi goals function - https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
   
-2.Return Negative - https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+// 2.Return Negative - https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
   return num > 0 ? -num : num;
 }
 
-3.Grasshopper - Terminal game move function - https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+// 3.Grasshopper - Terminal game move function - https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 function move (position, roll) {
   return position + roll * 2;
 }
 
-4.Grasshopper - Personalized Message - https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+// 4.Grasshopper - Personalized Message - https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 function greet (name, owner) {
   return name === owner ? 'Hello boss' : 'Hello guest';
 }
 
-5.Keep Hydrated! - https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+// 5.Keep Hydrated! - https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 function litres(time) {
   return Math.floor(time*0.5);
 }
 
-6.Opposites Attract - https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+// 6.Opposites Attract - https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 function lovefunc(flower1, flower2){
   return (flower1 + flower2)%2 !== 0;
 }
