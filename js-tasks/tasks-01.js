52960bea9809738851fb39a7107c1703d6266434
SawaAser <144454570+SawaAser@users.noreply.github.com>
2023-12-16 00:39:11 +0200

    done

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -37,5 +37,5 @@ function litres(time) {
 
 6.Opposites Attract - https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 function lovefunc(flower1, flower2){
-  return (flower1 + flower2)%2 !== 0 ? true : false;
+  return (flower1 + flower2)%2 !== 0;
 }
