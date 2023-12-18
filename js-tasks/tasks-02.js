23706f92f61f3699eafb3d4adb9739d054917337
Aleksandr <some_mail@mail.com>
2023-12-18 23:19:09 +0200

    code editing tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,7 +1,7 @@
-// Convert a string to an array 
+// Convert a string to an array
 // https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 
-const stringToArray = (string) => string.split(" ")
+const stringToArray = (string) => string.split(" ");
 
 // DNA to RNA Conversion
 // https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
@@ -14,23 +14,23 @@ function DNAtoRNA(dna) {
 //Find Maximum and Minimum Values of a List
 // https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
-const min = function(list){
+const min = function (list) {
   for (el of list) {
     if (el < list[0]) {
       list[0] = el;
     }
   }
   return list[0];
-}
+};
 
-const max = function(list) {
+const max = function (list) {
   for (el of list) {
     if (el > list[0]) {
       list[0] = el;
     }
   }
   return list[0];
-}
+};
 
 //Smallest value of an array
 // https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
@@ -42,6 +42,5 @@ function min(arr, toReturn) {
       minValue = num;
     }
   }
-  return toReturn === 'value' ? minValue : arr.indexOf(minValue);
+  return toReturn === "value" ? minValue : arr.indexOf(minValue);
 }
-
