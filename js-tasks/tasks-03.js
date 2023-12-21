8fd44adf243d80568b9849bf0eca3102213d517d
Aleksandr <some_mail@mail.com>
2023-12-21 12:47:35 +0200

    added inheritance task to tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -24,7 +24,7 @@ function giveMeFive(obj) {
   return myList;
 }
 
-// optional in-depth circuit problem 
+// OPTIONAL IN-DEPTH CIRCUIT PROBLEM
 
 //Understanding closures - the basics
 //https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
@@ -40,4 +40,32 @@ function buildFun(n) {
     res.push(savePosition(i));
   }
   return res;
+}
+
+//Fun with ES6 Classes #2 - Animals and Inheritance
+//https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
 }
