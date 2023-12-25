15511f6d91383e1e232bab5d5b3286e5cd57b2cf
Anna Poshyta <anna.poshyta@nure.ua>
2023-12-25 15:37:28 +0200

    update contact.delivery.js

--- js/contact.delivery.js
+++ js/contact.delivery.js
@@ -6,8 +6,7 @@ function formValidator(form) {
         errorLabel.classList.add('error-label');
         errorLabel.textContent = text;
         parent.classList.add('error');
-        parent.append(errorLabel)
-
+        parent.append(errorLabel);
     }
 
     function removeError(input) {
@@ -16,34 +15,75 @@ function formValidator(form) {
             parent.querySelector('.error-label').remove();
             parent.classList.remove('error');
         }
-
     }
-    let result = true;
 
+    function createSelectError(select, option) {
+        const parentSelect = select.closest('.input-box');
+        const errorLabelSelect = document.createElement('label');
+        errorLabelSelect.classList.add('error-label');
+        errorLabelSelect.textContent = option;
+        parentSelect.classList.add('error');
+        parentSelect.append(errorLabelSelect);
+    }
+
+    function removeSelectError(select) {
+        const parentSelect = select.closest('.input-box');
+        if (parentSelect.classList.contains('error')) {
+            parentSelect.querySelector('.error-label').remove();
+            parentSelect.classList.remove('error');
+        }
+    }
+    function createDateError(dateInput, text) {
+        const parentDate = dateInput.closest('.input-box');
+        const errorLabelDate = document.createElement('label');
+        errorLabelDate.classList.add('error-label');
+        errorLabelDate.textContent = text;
+        parentDate.classList.add('error');
+        parentDate.append(errorLabelDate);
+    }
+    
+    function removeDateError(dateInput) {
+        const parentDate = dateInput.closest('.input-box');
+        if (parentDate.classList.contains('error')) {
+            parentDate.querySelector('.error-label').remove();
+            parentDate.classList.remove('error');
+        }
+    }
+
+
+    let result = true;
     const allInputs = form.querySelectorAll('input');
 
     for (const input of allInputs) {
-
         removeError(input);
-
-        if (input.value == "" ) {
-        console.log("Ошибка поля");
-        createError(input, 'The field is not filled!');
-        result = false;
+        if (input.value === "") {
+            createError(input, 'The field is not filled!');
+            result = false;
         }
     }
-    return result
-}
-document.getElementById('add-form').addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    formValidator(this);
-    // Проводим валидацию формы
-    const isValid = formValidator(this);
-
-    // Если валидация успешна, отправляем форму
-    if (isValid) {
-        this.submit(); // Это отправит форму
+    const selectInput = form.querySelector('#choices');
+    removeSelectError(selectInput);
+    if (selectInput.value === "") {
+        createSelectError(selectInput, 'Choices Water type!');
+        result = false;
     }
+
+    const dateInput = form.querySelector('.select-date');
+    removeDateError(dateInput);
     
-})
+    if (!dateInput.value || dateInput.value === '2023-01-01') {
+        createDateError(dateInput, 'This field is required!');
+        result = false;
+    }
+    return result;
+}
+
+document.getElementById('add-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    const isValid = formValidator(this);
+    if (isValid) {
+        this.submit();
+    }
+});
