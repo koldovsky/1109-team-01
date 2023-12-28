9228e5936a8fea9cb51600c17710e61296087faa
Iryna Iasko <145124857+Irarasta@users.noreply.github.com>
2023-12-28 13:43:08 +0000

    carousel-clients totally  re-written

---
+++ js/carousel_clients.js
@@ -0,0 +1,45 @@
+const slides = [
+    '<div class="carousel-item"><img src="img/meijer.jpg"  alt="Meijer Logo"></div>',
+    '<div class="carousel-item"><img src="img/safeway.jpg"  alt="Safeway Logo"></div>',
+    '<div class="carousel-item"><img src="img/drager.jpg"  alt="Drager Logo"></div>',
+    '<div class="carousel-item"><img src="img/bexen.jpg"  alt="Bexen Logo"></div>',
+    '<div class="carousel-item"><img src="img/costco.jpg"  alt="Costco Logo"></div>',
+    '<div class="carousel-item"><img src="img/cdn.jpg"  alt="CDN Logo"></div>',
+];
+
+let currentSlideIdx = 0;
+
+function renderSlide() {
+    const slideContainer = document.querySelector('.products-carousel__slide');
+    slideContainer.innerHTML = '';
+
+    const numSlidesToShow = window.matchMedia('(min-width: 1200px)').matches ? 5 : 3;
+
+    for (let i = 0; i < numSlidesToShow; i++) {
+        const index = (currentSlideIdx + i) % slides.length;
+        const item = document.createElement('div');
+        item.className = 'carousel__item' + (i === 0 ? ' active' : '');
+        item.innerHTML = slides[index];
+        slideContainer.appendChild(item);
+    }
+}
+
+function nextSlide() {
+    currentSlideIdx = (currentSlideIdx + 1) % slides.length;
+    renderSlide();
+}
+
+function prevSlide() {
+    currentSlideIdx = (currentSlideIdx - 1 + slides.length) % slides.length;
+    renderSlide();
+}
+
+renderSlide();
+
+const buttonNext = document.querySelector('.products-carousel__button-next');
+buttonNext.addEventListener('click', nextSlide);
+
+const buttonPrev = document.querySelector('.products-carousel__button-prev');
+buttonPrev.addEventListener('click', prevSlide);
+
+window.addEventListener('resize', renderSlide);
