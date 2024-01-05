d6504ce9e4947c13f8a75f84b1738614df4c04ed
MorpheusFR <creatorsk@gmail.com>
2024-01-05 20:02:36 +0200

    Added ripple effects

--- js/delivery_ripples.js
+++ js/delivery_ripples.js
@@ -1,3 +1,15 @@
 $('.delivery').ripples({
     resolution: 400
 });
+
+$('.header__hero').ripples({
+    resolution: 400
+});
+
+$('.water-info').ripples({
+    resolution: 400
+});
+
+$('.main-contacts').ripples({
+    resolution: 400
+});
