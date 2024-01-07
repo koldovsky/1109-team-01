854c94c523ea2ce71297567fa1b60b1a40732ac8
Capylyn <120113983+Capylyn@users.noreply.github.com>
2024-01-07 13:36:25 +0000

    button update

--- js/scroll_page_button.js
+++ js/scroll_page_button.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var scrollLink = document.getElementById("scrollLink");
+    var scrollLinks = document.querySelectorAll(".scroll-link");
 
-    scrollLink.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent the default anchor behavior (e.g., navigating to a new page)
+    scrollLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
 
-        var targetSection = document.getElementById("targetSection");
-        
-        // Scroll to the target section
-        if (targetSection) {
-            targetSection.scrollIntoView({ behavior: "smooth" });
-        }
+            // Get the target section ID from the href attribute
+            var targetId = this.getAttribute("href").substring(1);
+            var targetSection = document.getElementById(targetId);
+
+            // Scroll to the target section
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: "smooth" });
+            }
+        });
     });
 });
